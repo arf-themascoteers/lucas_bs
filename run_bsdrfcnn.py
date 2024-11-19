0a5f2dc9dd3b825f29fc93db5eb3efa0eb75b73d
Arif <arif@themascoteers.com>
2024-11-20 00:46:43 +1100

    done

--- run_bsdrfcnn.py
+++ run_bsdrfcnn.py
@@ -1,13 +1,11 @@
 from task_runner import TaskRunner
 
 if __name__ == '__main__':
-    tag = "bsdrcnn_ks_crop"
+    tag = "bsdrcnn"
     tasks = {
         "algorithms" : ["bsdrcnn"],
-        "datasets": ["lucas_crop","lucas_crop_asa"],
-        #"datasets": ["lucas","lucas_crop","lucas_asa","lucas_crop_asa"],
-        #"target_sizes" : [20,50,100,200,300,500,600]
-        "target_sizes" : [200]
+        "datasets": ["lucas"],
+        "target_sizes" : [500]
     }
     ev = TaskRunner(tasks,1,tag,verbose=True)
     ev.evaluate()
