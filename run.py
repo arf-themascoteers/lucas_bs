679d678487ce976a97cd4f699624136f212edcd6
Arif <arif@themascoteers.com>
2024-11-19 18:28:55 +1100

    done

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from task_runner import TaskRunner
 
 if __name__ == '__main__':
-    tag = "pCrop"
+    tag = "pCrop50"
     tasks = {
         "algorithms" : ["bsdrcnn"],
-        "datasets": ["lucas_crop_r"],
+        "datasets": ["lucas_r"],
         "target_sizes" : [200]
     }
     ev = TaskRunner(tasks,1,tag,verbose=True)
