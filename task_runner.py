bc1432dd5f1023777ac07346df3fce88d8291a22
Arif <arif@themascoteers.com>
2024-11-19 17:15:17 +1100

    done

--- task_runner.py
+++ task_runner.py
@@ -20,7 +20,7 @@ class TaskRunner:
                 for target_size in self.task["target_sizes"]:
                     for fold, (train_x, train_y, test_x, test_y) in enumerate(dataset.get_k_folds()):
                         if self.reporter.record_exists(algorithm, dataset_name, target_size, fold):
-                            print(algorithm, "for", dataset, "for target size", target_size,"for fold", fold, "was done. Skipping")
+                            print(algorithm, "for", dataset.name, "for target size", target_size,"for fold", fold, "was done. Skipping")
                         else:
                             algorithm_object = Algorithm.create(algorithm, dataset, train_x, train_y,test_x,test_y, target_size, fold, self.reporter,self.verbose)
                             algorithm_object.compute_fold()
