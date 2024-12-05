43ce692b8a8ee1830f507e1f7f5d7630841027b1
Arif <arif@themascoteers.com>
2024-12-05 17:50:19 +1100

    done

--- run_r.py
+++ run_r.py
@@ -1,9 +1,9 @@
 from task_runner import TaskRunner
 
 if __name__ == '__main__':
-    tag = "check"
+    tag = "reltu"
     tasks = {
-        "algorithms" : ["bsdrfc_r", "bsdrcnn_r"],
+        "algorithms" : ["bsdrcnn_r","bsdrfc_r"],
         "datasets": ["lucas"],
         "target_sizes" : [8,16,32,64,128,256,512,1024,2048,500,4200],
         "scale_y" : ["robust"],
