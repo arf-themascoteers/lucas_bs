eddd44e8dcf365cea982d0a9fc2fa723a88184c0
Arif <arif@themascoteers.com>
2024-12-05 17:45:03 +1100

    done

--- run_r.py
+++ run_r.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     TEST = False
     tag = "check"
     tasks = {
-        "algorithms" : ["bsdrcnn_r","bsdrfc_r"],
+        "algorithms" : ["bsdrfc_r", "bsdrcnn_r"],
         "datasets": ["lucas"],
         "target_sizes" : [8,16,32,64,128,256,512,1024,2048,500,4200],
         "scale_y" : ["robust"],
