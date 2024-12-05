2e8d52ea690a266ef018163c2f6eb1fdd857d3b1
Arif <arif@themascoteers.com>
2024-12-05 11:44:17 +1100

    done

--- run_scale.py
+++ run_scale.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     tasks = {
         "algorithms" : ["bsdrfc"],
         "datasets": ["lucas"],
-        "target_sizes" : [16,50,100,200,300,500,1000,2000,4200],
+        "target_sizes" : [16,50,500],
         "scale_y" : ["robust","minmax"],
-        "mode" : ["static","dynamic","semi"]
+        "mode" : ["static"]
     }
     ev = TaskRunner(tasks,1,tag,verbose=True)
     ev.evaluate()
