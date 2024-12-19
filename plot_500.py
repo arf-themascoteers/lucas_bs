765afdeb521c0bcf8c7265bf99aa290e682524c2
Arif <arif@themascoteers.com>
2024-12-20 04:56:19 +1100

    done

---
+++ plot_500.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axes = plt.subplots(3, 2, figsize=(7, 7))
+#fig.subplots_adjust(hspace=0.5)
+data = pd.read_csv("v2.csv").to_numpy()
+
+for i in range(3):
+    row = data[i]
+    X = row[0:4200]
+    h = row[4200:4700]
+
+    axes[i,0].plot(X, color="green", label="Original data")
+    axes[i,0].set_title(f"Sample {i + 1}")
+    axes[i,0].set_xlabel("Band")
+    axes[i,0].set_ylabel("Reflectance")
+
+    axes[i,1].plot(h, color="red", label="Compressed data")
+    axes[i,1].set_title(f"Latent vector for sample {i + 1}")
+    axes[i,1].set_xlabel("Index")
+    axes[i,1].set_ylabel("Value")
+
+
+lines, labels = axes[0,1].get_legend_handles_labels()
+#fig.legend(lines, labels, loc='upper center', ncol=2,bbox_to_anchor=(0.5,1))
+
+#fig.subplots_adjust(top=0.9)
+plt.tight_layout()
+plt.savefig("low_rep_sae.png")
+plt.show()
+
