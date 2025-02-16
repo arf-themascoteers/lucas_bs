beb0c4bb6b3cd9b323f6ac44e48185363efaa710
Arif <arif@themascoteers.com>
2025-02-16 13:53:44 +1100

    done

--- run_asa.py
+++ run_asa.py
@@ -1,15 +1,18 @@
 from task_runner import TaskRunner
+from my_env import TEST
 
 if __name__ == '__main__':
-    tag = "asa_r"
+    train_sizes = [0.75]
+    verbose = False
+    tag = f"asa"
     tasks = {
         "algorithms" : ["asa"],
         "datasets": ["lucas_crop"],
         "target_sizes" : [500],
         "scale_y" : ["robust"],
-        "mode" : ["static"]
+        "mode" : ["dyn"],
+        "train_sizes":train_sizes
     }
     folds = 1
-    verbose = True
     ev = TaskRunner(tasks,folds,tag,verbose=verbose)
     ev.evaluate()
