633c36f5541010df655ec455d9828218fc9a58ec
Arif <arif@themascoteers.com>
2025-02-17 18:57:21 +1100

    done

--- run_model_time.py
+++ run_model_time.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     train_sizes = [0.75]
     verbose = False
     tasks = {
-        "algorithms" : ["bsdrcnn","bsdrfc","bsdrcnn_static","bsdrfc_static"],
+        "algorithms" : ["bsdrcnn","bsdrfc"],
         "datasets": ["lucas"],
         "target_sizes" : [8,16,32,64,128,256,512],
         "scale_y" : ["robust"],
-        "mode" : ["dyn"],
+        "mode" : ["dyn","static"],
         "train_sizes":train_sizes
     }
     folds = 1
