a2cc7ce2f89a56fffb7296b8f64d8937c630f4a5
Arif <arif@themascoteers.com>
2025-02-19 15:45:33 +1100

    done

--- ds_manager.py
+++ ds_manager.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler, RobustScaler
 
 
 class DSManager:
-    def __init__(self, name="lucas",folds=1,scale_y="robust",shuffle=True,train_size=0.75):
+    def __init__(self, name="lucas",folds=1,scale_y="robust",shuffle=True,train_size=0.75,split="normal"):
         self.name = name
         dataset_path = f"data/{self.name}.csv"
         df = pd.read_csv(dataset_path)
@@ -22,31 +22,14 @@ class DSManager:
         self.data[:,0:-1] = self.scaler_X.fit_transform(self.data[:,0:-1])
         self.data[:,-1] = self.scaler_y.fit_transform(self.data[:,-1].reshape(-1,1)).ravel()
         self.train_size = train_size
+        self.split = split
 
     def get_k_folds(self):
         for i in range(self.folds):
-            train_data, test_data = train_test_split(self.data, train_size=self.train_size, random_state=42+i)
+            if self.split == "normal":
+                train_data, test_data = train_test_split(self.data, train_size=self.train_size, random_state=42+i)
+            else:
+                train_data, test_data = ks_split(self.data, train_size=self.train_size)
             yield train_data[:,0:-1], train_data[:,-1], test_data[:,0:-1], test_data[:,-1]
 
 
-if __name__ == '__main__':
-    import numpy as np
-    ds = DSManager(folds=1, scale_y="minmax", name="lucas")
-    # pd.DataFrame(ds.data[:,-1], columns=['oc']).to_csv('oc.csv', index=False)
-    # print(np.mean(ds.data[:,-1]))
-    # for train_x, train_y, test_x, test_y in ds.get_k_folds():
-    #     print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
-    #     print(np.min(train_y))
-    #     print(np.max(train_y))
-    #     print(np.max(train_y)-np.min(train_y))
-    #     print(np.mean(train_y))
-    import matplotlib.pyplot as plt
-    data = np.mean(ds.data[:,0:-1],axis=0)
-    plt.plot(data)
-    plt.show()
-
-    dataset_path = f"data/lucas.csv"
-    data = pd.read_csv(dataset_path).to_numpy()[:,0:-1]
-    data = np.mean(data,axis=0)
-    plt.plot(data)
-    plt.show()
