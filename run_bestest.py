7e38ede3e06d49822607e7ba7f0738433da602fd
Arif <arif@themascoteers.com>
2025-02-26 15:23:05 +1100

    done

--- run_bestest.py
+++ run_bestest.py
@@ -1,11 +1,11 @@
 from task_runner import TaskRunner
 
 if __name__ == '__main__':
-    train_sizes = [0.75]
+    train_sizes = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
     verbose = True
-    tag = f"bestest"
+    tag = f"cnnvsfc"
     tasks = {
-        "algorithms": ["bsdrcnn_r"],
+        "algorithms": ["bsdrcnn_r","bsdrfc_r"],
         "datasets": ["lucas"],
         "target_sizes" : [128],
         "scale_y": ["robust"],
