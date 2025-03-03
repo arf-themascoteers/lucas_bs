71d20914fddc860d4ea88140e89f95a407868fe4
Arif <arif@themascoteers.com>
2025-03-04 06:33:29 +1100

    done

---
+++ run_bsdr_75.py
@@ -0,0 +1,17 @@
+from task_runner import TaskRunner
+
+if __name__ == '__main__':
+    train_sizes = [0.75]
+    verbose = False
+    tag = f"9_bsdr_75"
+    tasks = {
+        "algorithms": ["bsdrfc"],
+        "datasets": ["lucas"],
+        "target_sizes": [8, 16, 32, 64, 128, 256, 512],
+        "scale_y": ["robust"],
+        "mode": ["dyn"],
+        "train_sizes": train_sizes
+    }
+    folds = 1
+    ev = TaskRunner(tasks,folds,tag,verbose=verbose)
+    ev.evaluate()
