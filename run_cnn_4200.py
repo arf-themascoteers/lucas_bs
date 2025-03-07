28ec087afde47cde2907487746f76052c47ededd
arif <arif@themascoteers.com>
2025-03-07 18:59:30 +1100

    done

--- run_cnn_4200.py
+++ run_cnn_4200.py
@@ -3,7 +3,7 @@ from task_runner import TaskRunner
 if __name__ == '__main__':
     train_sizes = [0.75]
     verbose = False
-    tag = f"17_cnn_4200"
+    tag = f"18_cnn_4200"
     tasks = {
         "algorithms": ["cnn_4200"],
         "datasets": ["lucas"],
