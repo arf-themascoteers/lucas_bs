9cd581a8263ac9f93548c710e6366db814a249cf
arif <arif@themascoteers.com>
2025-03-13 18:02:47 +1100

    done

--- new_run/7_all_bands.py
+++ new_run/7_all_bands.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     tag = f"7_all_bands"
     tasks = {
         "algorithms": ["cnn_4200"],
-        "datasets": ["min_lucas"],
+        "datasets": ["lucas"],
         "target_sizes": [8, 16, 32, 64, 128, 256, 512],
         "scale_y": ["robust"],
         "mode": ["dyn"],
