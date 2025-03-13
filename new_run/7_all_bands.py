1bd008130fb3408ce4cc1998699ca06cd8ddd55f
arif <arif@themascoteers.com>
2025-03-13 18:36:34 +1100

    done

--- new_run/7_all_bands.py
+++ new_run/7_all_bands.py
@@ -4,13 +4,13 @@ import os
 os.chdir("..")
 
 if __name__ == '__main__':
-    train_sizes = [0.75]
+    train_sizes = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
     verbose = False
     tag = f"7_all_bands"
     tasks = {
         "algorithms": ["cnn_4200"],
         "datasets": ["lucas"],
-        "target_sizes": [8, 16, 32, 64, 128, 256, 512],
+        "target_sizes": [4200],
         "scale_y": ["robust"],
         "mode": ["dyn"],
         "train_sizes": train_sizes
