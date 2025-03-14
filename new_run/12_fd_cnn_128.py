6816966cf517bb3fa1763a31c098c0ab9350bf98
arif <arif@themascoteers.com>
2025-03-14 14:10:42 +1100

    done

--- new_run/12_fd_cnn_128.py
+++ new_run/12_fd_cnn_128.py
@@ -6,13 +6,13 @@ os.chdir("..")
 if __name__ == '__main__':
     train_sizes = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
     verbose = False
-    tag = f"11_best_128"
+    tag = f"12_fdcnn_128"
     tasks = {
         "algorithms": ["bsdrcnn_r"],
         "datasets": ["lucas"],
         "target_sizes": [128],
         "scale_y": ["robust"],
-        "mode": ["dyn"],
+        "mode": ["static"],
         "train_sizes": train_sizes
     }
     folds = 10
