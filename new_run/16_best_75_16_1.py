6fcc55fb5a05b11a8eaef4c18aaf409ddc7f5748
Arif <arif@themascoteers.com>
2025-03-15 16:05:46 +1100

    done`

--- new_run/16_best_75_16_1.py
+++ new_run/16_best_75_16_1.py
@@ -5,16 +5,16 @@ os.chdir("..")
 
 if __name__ == '__main__':
     train_sizes = [0.75]
-    verbose = False
-    tag = f"15"
+    verbose = True
+    tag = f"16_best_75_16_1"
     tasks = {
         "algorithms": ["bsdrcnn_r"],
         "datasets": ["lucas"],
-        "target_sizes": [128],
+        "target_sizes": [16],
         "scale_y": ["robust"],
         "mode": ["dyn"],
         "train_sizes": train_sizes
     }
-    folds = 10
+    folds = 1
     ev = TaskRunner(tasks,folds,tag,verbose=verbose)
     ev.evaluate()
