c18b2af561d55f99f4d44c069895c18dea5ded5a
Arif <arif@themascoteers.com>
2025-03-15 23:22:52 +1100

    done`

--- number_line.py
+++ number_line.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 
 def band_to_wl(band):
@@ -16,8 +17,14 @@ def band_str_to_wls(bands):
         wls.append(wl)
     return wls
 
-numbers1 = band_str_to_wls("")
-numbers2 = band_str_to_wls("")
+# numbers1 = band_str_to_wls("466|927|1389|1850|2312|2773|3235|3696")
+# numbers2 = band_str_to_wls("674|897|1411|2021|2308|2787|3467|3606")
+
+numbers1 = band_str_to_wls("249|493|737|982|1226|1470|1715|1959|2203|2448|2692|2936|3181|3425|3669|3914")
+numbers2 = band_str_to_wls("392|538|684|912|1138|1310|1674|2025|2136|2218|2630|2923|3473|3475|3664|3990")
+
+print(numbers1)
+print(numbers2)
 
 plt.figure(figsize=(10, 3))
 plt.hlines(2, 400, 2500, colors='black', linewidth=2)
@@ -35,4 +42,6 @@ plt.xlim(400, 2500)
 plt.ylim(0.5, 2.5)
 plt.axis('off')
 plt.legend()
+os.makedirs("wls", exist_ok=True)
+plt.savefig("wls/16.png")
 plt.show()
