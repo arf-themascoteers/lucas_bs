c402cf7e3253f72aca74281ee1d9fa7e179b030f
Arif <arif@themascoteers.com>
2025-04-18 13:14:37 +1000

    done

--- ga.py
+++ ga.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 plt.rcParams['font.family'] = 'Times New Roman'
 start = "466|927|1389|1850|2312|2773|3235|3696"
@@ -17,6 +18,7 @@ max_y = np.max(data) + 0.02
 
 plt.figure(figsize=(6, 2))
 plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
 plt.xticks([])
 plt.yticks([])
 plt.gca().spines[:].set_visible(False)
@@ -26,6 +28,7 @@ plt.close()
 
 plt.figure(figsize=(6, 2))
 plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
 plt.xticks([])
 plt.yticks([])
 plt.gca().spines[:].set_visible(False)
@@ -56,6 +59,7 @@ plt.close()
 
 plt.figure(figsize=(6, 2))
 plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
 plt.xticks([])
 plt.yticks([])
 plt.gca().spines[:].set_visible(False)
@@ -83,9 +87,64 @@ for i in range(len(x)):
 plt.savefig("plot_2_2.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
 plt.close()
 
+plt.figure(figsize=(6, 2))
+plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
+plt.xticks([])
+plt.yticks([])
+plt.gca().spines[:].set_visible(False)
+prev_end = end
+end = [900,1200,500,2300,1800,3500,3100,2600]
+x = end
+y = data[x]
+data_new = data.copy()
+data_new[prev_end] = data[prev_end]
+colors = plt.cm.Set1(np.linspace(0, 1, len(x)))
+plt.plot(data, color="black", linewidth=3)
+for i in range(len(x)):
+    plt.scatter(x[i], y[i], color=colors[i], s=200, zorder=5)
+plt.savefig("plot_2_3.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
+plt.close()
+
+plt.figure(figsize=(6, 1))
+plt.ylim([0, 1])
+plt.xlim([0, 4200])
+plt.xticks([])
+plt.yticks([])
+plt.gca().spines[:].set_visible(False)
+end = [900,1200,500,2300,1800,3500,3100,2600]
+x = end
+y = [0.5]*8
+colors = plt.cm.Set1(np.linspace(0, 1, len(x)))
+#plt.plot(data, color="black", linewidth=3)
+#random.shuffle(end)
+for i in range(len(x)):
+    plt.scatter(x[i], y[i], color=colors[i], s=200, zorder=5)
+plt.savefig("plot_2_4.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
+plt.close()
+
+plt.figure(figsize=(6, 1))
+plt.ylim([-0.1, 0.9])
+plt.xlim([0, 4200])
+plt.xticks([])
+plt.yticks([])
+plt.gca().spines[:].set_visible(False)
+end = [900,1200,500,2300,1800,3500,3100,2600]
+indices = np.argsort(end)
+x = end
+y = [0.1,0.3,0.5,0.2,0.7,0.3,0.6,0.1]
+x.sort()
+colors = plt.cm.Set1(np.linspace(0, 1, len(x)))
+
+plt.plot(x,y, color="black", linewidth=3)
+for i in range(len(x)):
+    plt.scatter(x[i], y[i], color=colors[indices[i]], s=200, zorder=5)
+plt.savefig("plot_2_5.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
+plt.close()
 
 plt.figure(figsize=(6, 2))
 plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
 plt.xticks([])
 plt.yticks([])
 plt.gca().spines[:].set_visible(False)
@@ -100,6 +159,7 @@ plt.close()
 
 plt.figure(figsize=(6, 2))
 plt.ylim([min_y, max_y])
+plt.xlim([0, 4200])
 plt.xticks([])
 plt.yticks([])
 plt.gca().spines[:].set_visible(False)
@@ -111,3 +171,36 @@ for i in range(len(x)):
     plt.scatter(x[i], y[i], color=colors[i], s=200, zorder=5)
 plt.savefig("plot_4.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
 plt.close()
+
+
+high = np.random.uniform(0.5, 1.0, size=10)
+low = np.random.uniform(0.0, 0.1, size=90)
+values = np.concatenate([high, low])
+np.random.shuffle(values)
+plt.figure(figsize=(6, 2))
+plt.ylim([-0.1, 1.1])
+plt.xlim([-1, 101])
+plt.xticks([])
+plt.yticks([])
+plt.gca().spines[:].set_visible(False)
+colors = plt.cm.Set1(np.linspace(0, 1, 100))
+for i in range(100):
+    plt.bar(i, values[i], color=colors[i])
+plt.savefig("plot_7.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
+plt.close()
+
+
+plt.figure(figsize=(6, 2))
+plt.ylim([-0.1, 1.1])
+plt.xlim([-1, 101])
+plt.xticks([])
+plt.yticks([])
+plt.gca().spines[:].set_visible(False)
+colors = plt.cm.Set1(np.linspace(0, 1, 100))
+downscaled = data.reshape(100, 42).mean(axis=1)
+recal = downscaled * values
+for i in range(100):
+    plt.bar(i, recal[i], color=colors[i])
+plt.savefig("plot_8.png", bbox_inches=plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted()), transparent=True)
+plt.close()
+
